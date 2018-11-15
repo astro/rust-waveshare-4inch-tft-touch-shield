11198322569ef58e0ea5b3fd44a753d8589b857a
Astro <astro@spaceboyz.net>
2018-11-15 23:01:10 +0100

    ili9486 command: -ReadInterfacePixelFormat, +...

--- src/display/ili9486/command.rs
+++ src/display/ili9486/command.rs
@@ -39,10 +39,83 @@ impl<'a> Command for MemoryWrite<'a> {
 
 simple_command!(SleepIn, 0x10);
 simple_command!(SleepOut, 0x11);
+simple_command!(PartialModeOn, 0x12);
 simple_command!(InversionOn, 0x21);
 simple_command!(InversionOff, 0x28);
 simple_command!(DisplayOn, 0x29);
 simple_command!(DisplayOff, 0x28);
+
+#[derive(Debug, Copy, Clone)]
+pub struct ColumnAddressSet {
+    /// Start X
+    pub sc: u16,
+    /// End X
+    pub ec: u16,
+}
+
+impl Command for ColumnAddressSet {
+    type Buffer = [u8; 4];
+
+    fn number() -> u8 {
+        0x2A
+    }
+
+    fn encode(self) -> Self::Buffer {
+        [(self.sc >> 8) as u8,
+         self.sc as u8,
+         (self.ec >> 8) as u8,
+         self.ec as u8,
+        ]
+    }
+}
+
+#[derive(Debug, Copy, Clone)]
+pub struct PageAddressSet {
+    /// Start Y
+    pub sp: u16,
+    /// End Y
+    pub ep: u16,
+}
+
+impl Command for PageAddressSet {
+    type Buffer = [u8; 4];
+
+    fn number() -> u8 {
+        0x2B
+    }
+
+    fn encode(self) -> Self::Buffer {
+        [(self.sp >> 8) as u8,
+         self.sp as u8,
+         (self.ep >> 8) as u8,
+         self.ep as u8,
+        ]
+    }
+}
+
+#[derive(Debug, Copy, Clone)]
+pub struct PartialArea {
+    /// Start Y
+    pub sr: u16,
+    /// End Y
+    pub er: u16,
+}
+
+impl Command for PartialArea {
+    type Buffer = [u8; 4];
+
+    fn number() -> u8 {
+        0x30
+    }
+
+    fn encode(self) -> Self::Buffer {
+        [(self.sr >> 8) as u8,
+         self.sr as u8,
+         (self.er >> 8) as u8,
+         self.er as u8,
+        ]
+    }
+}
 
 pub struct MemoryAccessControl {
     pub row_addr_order: bool,
@@ -112,18 +185,3 @@ impl Command for InterfacePixelFormat {
          (self.rgb_format as u8)]
     }
 }
-
-pub struct ReadInterfacePixelFormat;
-
-impl Command for ReadInterfacePixelFormat {
-    type Buffer = [u8; 2];
-
-    fn number() -> u8 {
-        0x0C
-    }
-
-    fn encode(self) -> Self::Buffer {
-        [0, 0]
-    }
-}
-
